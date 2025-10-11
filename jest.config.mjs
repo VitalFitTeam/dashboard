5986a12dbdd50cd7f2e9b557d5a2070d40d8c376
joseA <nobujose6@gmail.com>
2025-10-11 14:57:43 -0400

    hermaientas test

---
+++ jest.config.mjs
@@ -0,0 +1,22 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Proporciona la ruta a tu aplicación Next.js para cargar next.config.js y .env en el entorno de prueba
+  dir: './',
+});
+
+// Añade cualquier configuración personalizada de Jest que desees
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    // Manejar alias de módulo (esto debe coincidir con tsconfig.json)
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // Manejar mocks de CSS
+    '^.+\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  preset: 'ts-jest',
+};
+
+// createJestConfig se exporta de esta manera para asegurar que next/jest pueda cargar la configuración de Next.js
+export default createJestConfig(customJestConfig);
---
+++ jest.setup.js
@@ -0,0 +1,2 @@
+// Este archivo importa las aserciones extendidas para Jest, como .toBeInTheDocument()
+import "@testing-library/jest-dom";
