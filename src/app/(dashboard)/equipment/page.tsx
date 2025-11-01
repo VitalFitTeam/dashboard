de47b8afb27d30745c8da71fcd3e8894e9722c7b
Inf10sophia <1001.28055963.ucla@gmail.com>
2025-11-01 12:07:46 -0400

    implementacion del CRUD para equipamientos

--- src/app/(dashboard)/equipment/page.tsx
+++ src/app/(dashboard)/equipment/page.tsx
@@ -2,19 +2,54 @@
 import { PageHeader } from "@/components/ui/PageHeader";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "@heroicons/react/24/outline";
+import type { Equipment } from "@/models/equipment";
+import CreateEquipment from "./CreateEquipment";
+import EditEquipment from "./EditEquipment";
+import ViewDetailsEquipment from "./ViewDetailsEquipment";
 import EquipmentTable from "./EquipmentTable";
+import { useState } from "react";
 
-export default function Users() {
+export default function Equipment() {
+  const [showCreateForm, setShowCreateForm] = useState(false);
+  const [editingEquipment, setEditingEquipment] = useState<Equipment | null>(
+    null,
+  );
+  const [viewEquipment, setViewEquipment] = useState<Equipment | null>(null);
+
+  if (showCreateForm) {
+    return <CreateEquipment onBack={() => setShowCreateForm(false)} />;
+  }
+
+  if (editingEquipment) {
+    return (
+      <EditEquipment
+        equipment={editingEquipment}
+        onBack={() => setEditingEquipment(null)}
+      />
+    );
+  }
+
+  if (viewEquipment) {
+    return (
+      <ViewDetailsEquipment
+        equipment={viewEquipment}
+        onBack={() => setViewEquipment(null)}
+      />
+    );
+  }
+
   return (
     <div className="flex-1 space-y-8 p-8 pt-6">
       <PageHeader title="EQUIPAMIENTO">
-        <Button variant="primary">
+        <Button variant="primary" onClick={() => setShowCreateForm(true)}>
           <PlusIcon className="h-5 w-5" />
           Agregar Equipamiento
         </Button>
       </PageHeader>
-
-      <EquipmentTable />
+      <EquipmentTable
+        onView={(equipment) => setViewEquipment(equipment)}
+        onEdit={(equipment) => setEditingEquipment(equipment)}
+      />
     </div>
   );
 }
