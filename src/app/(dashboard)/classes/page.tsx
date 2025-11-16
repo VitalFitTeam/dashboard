549a429d247de765b16869fb5f018da7d2627d0c
AlbaniBarragan <155846628+albanibarragan@users.noreply.github.com>
2025-11-16 17:56:43 -0400

    feat(calendar): refactor CalendarWrapper to use page navigation

--- src/app/(dashboard)/classes/page.tsx
+++ src/app/(dashboard)/classes/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState, useCallback } from "react";
 import { PageHeader } from "@/components/ui/PageHeader";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { ClassCalendar } from "@/components/calendar/ClassCalendar";
 
 export default function ClassesMethodsPage() {
   const { token } = useAuth();
@@ -16,15 +16,14 @@ export default function ClassesMethodsPage() {
 
   return (
     <div className="flex-1 space-y-8 p-8 pt-6">
-      <PageHeader title="CLASES">
-        <Button
-          className="bg-transparent text-black border border-gray-100"
-          onClick={() => router.push("/classes/new")}
-        >
-          <PlusIcon className="h-5 w-5" />
-          Agregar una clase
-        </Button>
-      </PageHeader>
+      <PageHeader
+        title="Calendario de Clases"
+        subtitle="Gestiona clases, reservas y horarios del gimnasio."
+      ></PageHeader>
+
+      <div className="bg-background p-4 rounded-lg shadow-sm">
+        <ClassCalendar />
+      </div>
     </div>
   );
 }
