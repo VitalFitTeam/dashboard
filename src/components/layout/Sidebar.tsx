382d91698397eccf8d7c6488eac5cba46f536229
Yovani Navas <65320280+yjnavas@users.noreply.github.com>
2025-11-30 16:09:55 -0400

    Gestión de Banners Publicitarios (Marketing) (#90)
    * feat(banner): conectar banner con la API
    
    * fix: actualización de ruta en Sidebar.tsx
    
    ---------
    
    Co-authored-by: raphaell22 <rafaelcarmona295@gmail.com>

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -108,6 +108,7 @@ const sidebarMenusByRole: Record<string, NavSection[]> = {
             { name: "Instructores", href: "/instructors" },
             { name: "Promociones", href: "/promotions" },
             { name: "Paquetes", href: "/packages" },
+            { name: "Gestión Global de Banners ", href: "/banners" },
           ],
         },
 
