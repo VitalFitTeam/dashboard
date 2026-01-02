230492252586a3eb53856ec4cc52bda1b63d2733
AlbaniBarragan <155846628+albanibarragan@users.noreply.github.com>
2026-01-01 23:17:34 -0400

    feature: reporte de ventas

---
+++ src/hooks/reports/useSalesReports.ts
@@ -0,0 +1,95 @@
+import { api } from "@/lib/sdk-config";
+import { useReportBase } from "./useReportBase";
+import { ChartData, KPICard, TopBranch, TotalSalesStats } from "@vitalfit/sdk";
+
+export const useSalesReports = {
+
+  useTotalSales: (jwt: string | null) =>
+    useReportBase<TotalSalesStats>(
+      jwt ? ["sales", "totalSales", jwt] : null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.totalSales(jwt);
+      }
+    ),
+
+    useAverageTicket: (jwt: string | null, branchId?: string) =>
+    useReportBase<KPICard>(
+      jwt ? ["sales", "averageTicketKPI", jwt, branchId] : null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.averageTicketKPI(jwt, branchId);
+      }
+    ),
+
+     useTotalTransactionsKPI: (jwt: string | null, branchId?: string) =>
+    useReportBase<KPICard>(
+      jwt ? ["sales", "totalTransactionsKPI", jwt, branchId] : null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.totalTransactionsKPI(jwt, branchId);
+      }
+    ),
+
+    useGlobalStats: (jwt: string | null ) =>
+    useReportBase<TopBranch[]>(
+      jwt ? ["sales", "totalTransactionsKPI", jwt] : null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.globalStats(jwt);
+      }
+    ),
+
+    useSalesByCategory: (jwt: string | null,  start: string, end: string) =>
+    useReportBase<ChartData[]>(
+      jwt && start && end ? ["sales", "salesByCategory", jwt, start, end]: null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.salesByCategory(jwt, start, end);
+      }
+    ),
+
+    useSalesByPaymentMethod: (jwt: string | null,  start: string, end: string) =>
+    useReportBase<ChartData[]>(
+      jwt && start && end ? ["sales", "salesByPaymentMethod", jwt, start, end]: null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.salesByPaymentMethod(jwt, start, end);
+      }
+    ),
+
+    useSalesByHour: (jwt: string | null,  start: string, end: string) =>
+    useReportBase<ChartData[]>(
+      jwt && start && end ? ["sales", "salesByHour", jwt, start, end]: null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.salesByHour(jwt, start, end);
+      }
+    ),
+
+    useTopBranches: (jwt: string | null) =>
+    useReportBase<ChartData[]>(
+      jwt ? ["sales", "topBranches", jwt]: null,
+      () => {
+        if (!jwt) {
+            throw new Error("No JWT provided");
+        }
+        return api.report.topBranches(jwt);
+      }
+    ),
+
+};
